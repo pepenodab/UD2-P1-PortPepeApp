893a71b44aa6b3d558de17886e9b84d7b1cbb0f0
___pepe_ <usr3854@salesianos-lacuesta.net>
2024-11-10 04:30:01 +0000

    feat: Welcome components

---
+++ styles/colors.ts
@@ -0,0 +1,10 @@
+export const COLOR = {
+  primary_LIGHT: "#f0ece4",
+  secondary_LIGHT: "#08cc44",
+  title_LIGHT: "#1e1e1e",
+  secondarytitle_LIGHT: "#ae7e47",
+  primary_DARK: "#3d3d3d",
+  secondary_DARK: "#0d6135",
+  title_DARK: "#d3d3d3",
+  secondarytitle_DARK: "#8b4513",
+};
