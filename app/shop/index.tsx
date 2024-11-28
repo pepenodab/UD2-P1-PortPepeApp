8a83892e51fdd1fe65b3480c47d8ab353825a7e3
___pepe_ <usr3854@salesianos-lacuesta.net>
2024-11-28 17:59:20 +0000

    feat: add the function to add a product

--- app/shop/index.tsx
+++ app/shop/index.tsx
@@ -5,14 +5,65 @@ import {
   StyleSheet,
   Pressable,
   Modal,
+  TextInput,
 } from "react-native";
 import React, { useState } from "react";
 import { products } from "../../data/productsList";
 import Product from "../../components/Product";
 import { COLOR } from "../../styles/colors";
 import { ProductClass } from "../../model/Product";
+import RNPickerSelect from "react-native-picker-select";
+import uuid from "react-native-uuid";
+import "react-native-get-random-values";
 
 const listSHop = () => {
+  const [notFull, setNotFull] = useState(true);
+
+  const [newProduct, setNewProduct] = useState<ProductClass>({
+    id: uuid.v4(),
+    name: "",
+    category: "",
+    quantity: 0,
+    price: 0,
+    onCart: true,
+  });
+
+  const handleAdd = () => {
+    if (newProduct.name.trim().length == 0) {
+      setNotFull(true);
+      alert("No se puede añadir objetos con campos vacios");
+      return;
+    }
+    if (newProduct.category.trim().length == 0 || newProduct.category == null) {
+      setNotFull(true);
+      alert("No se puede añadir objetos con campos vacios");
+      return;
+    }
+    if (newProduct.quantity == 0) {
+      setNotFull(true);
+      alert("No se puede añadir objetos con campos vacios");
+      return;
+    }
+    if (newProduct.price == 0) {
+      setNotFull(true);
+      alert("No se puede añadir objetos con campos vacios");
+      return;
+    }
+
+    const productToAdd = { ...newProduct };
+
+    setProductList([...productList, productToAdd]);
+
+    setNewProduct({
+      id: uuid.v4(),
+      name: "",
+      category: "",
+      quantity: 0,
+      price: 0,
+      onCart: true,
+    });
+  };
+
   const [showModal, setShowModal] = useState(false);
   const handleModal = () => {
     setShowModal(!showModal);
@@ -37,21 +88,83 @@ const listSHop = () => {
       <Text style={styles.title}>LISTA DE COMPRA</Text>
 
       <Modal transparent={true} animationType="slide" visible={showModal}>
-        <View
-          style={{
-            backgroundColor: "rgba(255, 255, 255, 0.5)",
-            height: "100%",
-          }}
-        >
-          <Text>Hola</Text>
-          <Pressable
-            style={styles.add}
-            onPress={() => {
-              handleModal();
-            }}
-          >
-            <Text style={styles.text_add}>Añadir...</Text>
-          </Pressable>
+        <View style={styles.modal_container}>
+          <View style={styles.form_container}>
+            <View style={styles.form_row}>
+              <Text style={styles.letter_form}>Nombre: </Text>
+              <TextInput
+                placeholder="Nombre..."
+                style={styles.input_form}
+                value={newProduct.name}
+                onChangeText={(text) =>
+                  setNewProduct({ ...newProduct, name: text })
+                }
+              />
+            </View>
+            <View style={styles.form_row}>
+              <Text style={styles.letter_form}>Categoría:</Text>
+              <RNPickerSelect
+                placeholder={{ label: "Selecciona una categoría", value: null }}
+                style={{ inputAndroid: { ...styles.input_form } }}
+                value={newProduct.category}
+                onValueChange={(value) =>
+                  setNewProduct({ ...newProduct, category: value })
+                }
+                items={[
+                  { label: "Panederia", value: "bakery" },
+                  { label: "Enlatado", value: "canned" },
+                  { label: "Bebida", value: "drink" },
+                  { label: "Pescado", value: "fish" },
+                  { label: "Fruta o vegetal", value: "fruitveggie" },
+                  { label: "Carne", value: "meat" },
+                  { label: "Otro...", value: "other" },
+                ]}
+              />
+            </View>
+            <View style={styles.form_row}>
+              <Text style={styles.letter_form}>Cantidad: </Text>
+              <TextInput
+                placeholder="Cantidad..."
+                style={styles.input_form}
+                keyboardType="numeric"
+                value={newProduct.quantity.toString()}
+                onChangeText={(quantity) =>
+                  setNewProduct({ ...newProduct, quantity: parseInt(quantity) })
+                }
+              />
+            </View>
+            <View style={styles.form_row}>
+              <Text style={styles.letter_form}>Precio u/:</Text>
+              <TextInput
+                placeholder="Precio u/..."
+                style={styles.input_form}
+                keyboardType="numeric"
+                value={
+                  newProduct.price === 0 ? "" : newProduct.price.toString()
+                }
+                onChangeText={(price) =>
+                  setNewProduct({ ...newProduct, price: parseFloat(price) })
+                }
+              />
+            </View>
+            <Pressable
+              style={styles.add}
+              onPress={() => {
+                handleAdd();
+                handleModal();
+              }}
+            >
+              <Text style={styles.text_add}>Añadir...</Text>
+            </Pressable>
+            <Pressable
+              style={styles.exit}
+              onPress={() => {
+                handleModal();
+              }}
+            >
+              <Text style={styles.text_add}>Cancelar</Text>
+            </Pressable>
+          </View>
         </View>
       </Modal>
 
@@ -133,5 +246,50 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     marginTop: 6,
+  },
+  modal_container: {
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    height: "100%",
+  },
+  form_container: {
+    backgroundColor: COLOR.primary_LIGHT,
+    display: "flex",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    marginTop: "45.5%",
+    height: "60%",
+    width: "95%",
+    marginLeft: "3%",
+    borderColor: COLOR.secondary_LIGHT,
+    borderWidth: 5,
+    borderRadius: 50,
+  },
+  input_form: {
+    backgroundColor: COLOR.secondary_LIGHT,
+    color: "white",
+    width: 150,
+    height: 50,
+    paddingHorizontal: 8,
+    borderRadius: 5,
+  },
+  form_row: {
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  letter_form: {
+    fontSize: 16,
+    color: COLOR.secondarytitle_LIGHT,
+  },
+  exit: {
+    marginTop: 20,
+    borderWidth: 2,
+    backgroundColor: "red",
+    borderColor: "red",
+    width: 130,
+    height: 40,
   },
 });
