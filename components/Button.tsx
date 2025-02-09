52aa4d0a2022961b6d52ffb06a6a4619cee6293a
___pepe_ <usr3854@salesianos-lacuesta.net>
2025-02-09 19:42:33 +0000

    feat(Camera): Create camera functions to take pictures

---
+++ components/Button.tsx
@@ -0,0 +1,30 @@
+// import * as React from "react";
+// import { StyleSheet, Text, TouchableOpacity } from "react-native";
+// import { MaterialIcons } from "@expo/vector-icons";
+
+// const Button = ({
+//   icon = String,
+//   size = Number,
+//   color = String,
+//   style = String,
+// }) => {
+//   return (
+//     <TouchableOpacity>
+//       <MaterialIcons
+//         name={icon}
+//         size={size ? size : 20}
+//         color={color ? color : "#f1f1f1"}
+//       />
+//     </TouchableOpacity>
+//   );
+// };
+
+// export default Button;
+
+// const styles = StyleSheet.create({
+//   button: {
+//     height: 40,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
